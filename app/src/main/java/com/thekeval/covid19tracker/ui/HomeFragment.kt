273b1d1f010113bb1903a9de05fd42d845654203
Keval <keval.langalia@live.com>
2021-02-18 03:40:14 +0530

    midway - creating adapter

--- app/src/main/java/com/thekeval/covid19tracker/ui/HomeFragment.kt
+++ app/src/main/java/com/thekeval/covid19tracker/ui/HomeFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.thekeval.covid19tracker.databinding.FragmentHomeBinding
+import com.thekeval.covid19tracker.domain.DomainModel
 
 class HomeFragment: Fragment() {
 
@@ -26,3 +28,15 @@ class HomeFragment: Fragment() {
 
 
 }
+
+class SummaryClicked(val block: (DomainModel) -> Unit) {
+    fun onClick(model: DomainModel) = block(model)
+}
+
+//class CovidSummaryAdapter(val callback: SummaryClicked) : RecyclerView.Adapter<CovidSummaryViewHolder>() {
+//
+//}
+//
+//class CovidSummaryViewHolder: RecyclerView.ViewHolder {
+//
+//}
