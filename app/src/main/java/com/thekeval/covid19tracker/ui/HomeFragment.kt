24dec8e19a519d77506f2a5bf6848470570d6d0c
Keval <keval.langalia@live.com>
2021-02-18 04:04:02 +0530

    fragment related code done

--- app/src/main/java/com/thekeval/covid19tracker/ui/HomeFragment.kt
+++ app/src/main/java/com/thekeval/covid19tracker/ui/HomeFragment.kt
@@ -4,14 +4,32 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.thekeval.covid19tracker.R
 import com.thekeval.covid19tracker.databinding.FragmentHomeBinding
+import com.thekeval.covid19tracker.databinding.SummaryItemBinding
 import com.thekeval.covid19tracker.domain.DomainModel
+import com.thekeval.covid19tracker.viewmodel.HomeViewModel
 
 class HomeFragment: Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
+
+    private val viewmodel: HomeViewModel by lazy {
+        val activity = requireNotNull(this.activity) {
+            "You can only access the viewModel after onViewCreated()"
+        }
+
+        ViewModelProvider(this, HomeViewModel.Factory(activity.application)).get(HomeViewModel::class.java)
+    }
+
+    private lateinit var summaryAdapter: CovidSummaryAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,23 +38,72 @@ class HomeFragment: Fragment() {
     ): View? {
 
         binding = FragmentHomeBinding.inflate(inflater)
+        binding.lifecycleOwner = this
 
+        binding.viewmodel = viewmodel
+
+        summaryAdapter = CovidSummaryAdapter(SummaryClicked {
+            Toast.makeText(context, "Incoming feature..", Toast.LENGTH_LONG)
+        })
+
+        binding.summaryList.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = summaryAdapter
+        }
 
 
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        
+    }
+
+
+}
+
+
+class CovidSummaryAdapter(val callback: SummaryClicked) : RecyclerView.Adapter<CovidSummaryViewHolder>() {
+
+    var summaries: List<DomainModel> = emptyList()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CovidSummaryViewHolder {
+        // val withDataBinding = SummaryItemBinding.inflate(LayoutInflater.from(parent.context))
+        val withDataBinding: SummaryItemBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            CovidSummaryViewHolder.LAYOUT,
+            parent,
+            false)
+
+        return CovidSummaryViewHolder(withDataBinding)
+    }
+
+    override fun onBindViewHolder(holder: CovidSummaryViewHolder, position: Int) {
+        holder.binding.also {
+            it.summary = summaries[position]
+            it.summaryCallback = callback
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return summaries.size
+    }
+
+}
+
+class CovidSummaryViewHolder(val binding: SummaryItemBinding) : RecyclerView.ViewHolder(binding.root) {
+    companion object {
+        @LayoutRes
+        val LAYOUT = R.layout.summary_item
+    }
 }
 
 class SummaryClicked(val block: (DomainModel) -> Unit) {
     fun onClick(model: DomainModel) = block(model)
 }
-
-//class CovidSummaryAdapter(val callback: SummaryClicked) : RecyclerView.Adapter<CovidSummaryViewHolder>() {
-//
-//}
-//
-//class CovidSummaryViewHolder: RecyclerView.ViewHolder {
-//
-//}
